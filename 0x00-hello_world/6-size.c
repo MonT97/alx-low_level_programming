e09088e236886a04361972e1b771871e43a59a95
MonT97 <monked1997@gmail.com>
2022-12-08 20:52:39 -0800

    task_6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+
+	printf("Size of char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of float: %lu bytes(s)\n", sizeof(float));
+
+	return (0);
+
+}

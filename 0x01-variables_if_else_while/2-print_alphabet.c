a0e792c3e37411f69f26d4bd73ece5845b502c1b
MonT97 <monked1997@gmail.com>
2022-12-11 12:14:06 -0800

    task_2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - the main function of the program
+ * Return: an integr
+ */
+
+int main(void)
+{
+	char chr = 'a';
+
+	while (chr <= 'z')
+	{
+		putchar(chr);
+		chr++;
+	}
+
+	chr = '\n';
+	putchar(chr);
+
+	return (0);
+}

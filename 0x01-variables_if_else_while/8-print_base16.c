76a0710c81413b6dd6ea1555c5894095a0cf4952
MonT97 <monked1997@gmail.com>
2022-12-11 14:48:39 -0800

    task_8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - the main function of the program
+ * Return: an integr
+ */
+
+int main(void)
+{
+
+	char chr;
+
+	for (chr = '0'; chr <= '9'; chr++)
+	{
+		putchar(chr);
+	}
+
+	for (chr = 'a'; chr <= 'f'; chr++)
+	{
+		putchar(chr);
+	}	
+	putchar('\n');
+
+	return (0);
+
+}
